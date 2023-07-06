d61e912d33952b8ceea1a94ebab8add371d152d6
Carbon.lv <130360750+carbon-portkey@users.noreply.github.com>
2023-07-06 15:03:21 +0800

    feat: init

---
+++ app/src/main/java/com/example/portkeysdk/Props/Global.java
@@ -0,0 +1,5 @@
+package com.example.portkeysdk.Props;
+
+public interface Global {
+    String PEER_HTTP_URL="http://192.168.66.61:8000";
+}
---
+++ app/src/main/java/com/example/portkeysdk/init/GlobalConfig.java
@@ -0,0 +1,27 @@
+package com.example.portkeysdk.init;
+
+import com.example.portkeysdk.Props.Global;
+import io.aelf.sdkv2.AElfClientV2;
+
+public class GlobalConfig {
+    private static final GlobalConfig ins;
+
+    static {
+        ins = new GlobalConfig();
+    }
+
+    public static GlobalConfig getIns() {
+        return ins;
+    }
+
+    private GlobalConfig() {
+        client = new AElfClientV2(Global.PEER_HTTP_URL);
+    }
+
+    public AElfClientV2 getClient() {
+        return client;
+    }
+
+    private AElfClientV2 client;
+
+}
